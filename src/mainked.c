b09e4d67b6f8c9f8df3fb2de933da2846613622b
Chad Estioco <chadestioco@gmail.com>
2024-10-22 14:36:42 +0200

    :sparkles: documentation :sparkles:

--- src/mainked.c
+++ src/mainked.c
@@ -15,9 +15,42 @@ void enableRawMode() {
     atexit(disableRawMode);
     struct termios raw = orig_termios;
     tcgetattr(STDIN_FILENO, &raw);
+    /*
+     * Flags turned _off_:
+     *
+     * ICRNL - allows Ctrl-M to be read as Carriage Return/New Line.
+     *
+     * IXON - input transmission on. With this turned off, no data is
+     * transmitted to the terminal at all. Ctrl-S triggers XOFF (pause
+     * transmission) while Ctrl-Q triggers XON (resume transmission).
+     */
     raw.c_iflag &= ~(ICRNL | IXON);
+    /*
+     * Flags turned _off_:
+     *
+     * OPOST - terminals interpret the ENTER keystroke as a combination of
+     * Carriage Return/New Line automatically. This is know as output
+     * processing. With this on '\n' is also CR/NL. With this off, we have to
+     * manually issue an accompanying '\r'.
+     */
     raw.c_oflag &= ~(OPOST);
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    /*
+     * Flags turned _off_:
+     *
+     * ECHO - causes keystrokes to be printed (echoed) to the terminal in the
+     * normal way (i.e., like you would expect a typewriter to work).
+     *
+     * ICANON - reads input in the canonical mode (line-by-line). With this off
+     * we can read input byte-by-byte.
+     *
+     * IEXTEN - in some terminals, Ctrl-C allows you to "materialize" the next
+     * input; try Ctrl-C then [ENTER] in bash (or any other non-alphanumeric
+     * key). This flag handles that.
+     *
+     * ISIG - reads input signals. With this off, the program can ignore Ctrl-C
+     * (SIGINT) and Ctrl-Z (SIGSTOP).
+     */
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
